83e62548b6e021a4f06931c57355f02e58acaf1f
Syd-SydneyJr <127783310+Syd-SydneyJr@users.noreply.github.com>
2025-03-31 00:12:57 -0500

    Update main.py

--- Python_Code/main.py
+++ Python_Code/main.py
@@ -91,7 +91,7 @@ def network_frame():
 def create_entry_grid(red_frame,green_frame):
     root.after(3000,network_frame)
 
-    for row in range(2,23):  # 20 rows
+    for row in range(2,23):  #20 rows
         red_details = [tk.StringVar(),tk.StringVar()]
         red_values[row-1] = red_details
 
@@ -99,14 +99,14 @@ def create_entry_grid(red_frame,green_frame):
         green_values[row-1] = green_details
 
         red_entry = tk.Entry(red_frame, width=25, textvariable=red_details[0])
-        red_entry.grid(row=row+1, column=0, padx=10, pady=5, sticky="ew")
+        red_entry.grid(row=row+1, column=0, padx=10, pady=4, sticky="ew")
         red_entry = tk.Entry(red_frame, width=25, textvariable=red_details[1])
-        red_entry.grid(row=row+1, column=1, padx=10, pady=5, sticky="ew")
+        red_entry.grid(row=row+1, column=1, padx=10, pady=4, sticky="ew")
 
         green_entry = tk.Entry(green_frame, width=25, textvariable=green_details[0])
-        green_entry.grid(row=row+1, column=0, padx=10, pady=5, sticky="ew")
+        green_entry.grid(row=row+1, column=0, padx=10, pady=4, sticky="ew")
         green_entry = tk.Entry(green_frame, width=25, textvariable=green_details[1])
-        green_entry.grid(row=row+1, column=1, padx=10, pady=5, sticky="ew")
+        green_entry.grid(row=row+1, column=1, padx=10, pady=4, sticky="ew")
 
 create_entry_grid(red_base,green_base)
 
