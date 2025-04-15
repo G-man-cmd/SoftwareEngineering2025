1b50fb3cff318db040a52348da6ba626844841c4
Syd-SydneyJr <127783310+Syd-SydneyJr@users.noreply.github.com>
2025-04-15 16:23:03 -0500

    Update server.py

--- Python_Code/server.py
+++ Python_Code/server.py
@@ -3,14 +3,13 @@ import threading
 from datetime import datetime
 
 class Server:
-    def __init__(self, recv_addr="0.0.0.0",recv_port = 7501, bcast_addr="255.255.255.255", bcast_port=7500,game_obj=None,scrollframe_obj=None):
+    def __init__(self, recv_addr="0.0.0.0",recv_port = 7501, bcast_addr="255.255.255.255", bcast_port=7500,controller=None):
         self.recv_addr = recv_addr
         self.recv_port = recv_port
         self.bcast_addr = bcast_addr
         self.bcast_port = bcast_port
-        self.game_obj = game_obj
-        self.scrollframe_obj = scrollframe_obj
         self.running = True
+        self.controller = controller
 
         # Initialize listening socket
         self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,6 +18,46 @@ class Server:
         # Initialize broadcast socket
         self.bcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.bcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    def update_scores(self):
+        this_game = self.controller.this_game
+
+        green_live_tree = self.controller.frames['pregame_screen'].green_live_tree
+        
+        for item in green_live_tree.get_children():
+            row_data = green_live_tree.item(item)["values"]
+            player = this_game.get_player_by_hardware_id(str(row_data[1]))
+            new_score = player.score
+            green_live_tree.item(item, values=(green_live_tree.item(item)['values'][0], 
+                                            green_live_tree.item(item)['values'][1], 
+                                            new_score))
+
+        red_live_tree = self.controller.frames['pregame_screen'].red_live_tree
+
+        for item in red_live_tree.get_children():
+            row_data = red_live_tree.item(item)["values"]
+            player = this_game.get_player_by_hardware_id(str(row_data[1]))
+            new_score = player.score
+            red_live_tree.item(item, values=(red_live_tree.item(item)['values'][0], 
+                                            red_live_tree.item(item)['values'][1], 
+                                            new_score))
+
+        green_total = self.controller.this_game.total_team_score('G')
+        self.controller.frames['pregame_screen'].label_green_total.config(text = f"Total Score:{green_total}")
+
+        red_total = self.controller.this_game.total_team_score('R')
+        self.controller.frames['pregame_screen'].label_red_total.config(text = f"Total Score:{red_total}")
+
+        if green_total > red_total:
+            self.controller.frames['pregame_screen'].label_green_total.config(fg = 'yellow')
+            self.controller.frames['pregame_screen'].label_red_total.config(fg = 'white')
+        elif green_total == red_total:
+            self.controller.frames['pregame_screen'].label_green_total.config(fg = 'white')
+            self.controller.frames['pregame_screen'].label_red_total.config(fg = 'white')
+        else:
+            self.controller.frames['pregame_screen'].label_red_total.config(fg = 'yellow')
+            self.controller.frames['pregame_screen'].label_green_total.config(fg = 'white')
+
 
     def listen(self):
 
@@ -31,47 +70,36 @@ class Server:
                 data, client_address = self.recv_socket.recvfrom(32)
                 if data:
                     data = data.decode('utf-8').strip('\n')
+                    shooter_id, target_id = data.split(':')
 
-                    if ":" in data and self.running:
-                        shooter_id, target_id = data.split(':')
-                        
-                        try:
-                            results = self.game_obj.get_player_by_hardware_id(shooter_id)
-                            results.update_score(points = 10)
-                            shooter_name = results.player_name
-                            shooter_team = results.team_name
-                            shooter_score = results.score
-                            shooter_id = results.player_id
+                    shooter = self.controller.this_game.get_player_by_hardware_id(shooter_id)
+                    target = self.controller.this_game.get_player_by_hardware_id(target_id)
+                    
 
-                            results = self.game_obj.get_player_by_hardware_id(target_id)
-                            target_name = results.player_name
-                            target_team = results.team_name
-                            target_score = results.score
-                            target_id = results.player_id
-
-
-
-                            
-
-
-                        except IndexError as e:
-                            pass
-                        
-                        self.scrollframe_obj.add_text(f'[/] {datetime.now().time()} : {shooter_name} has hit {target_name} | Shutting down {target_name} for 10 seconds')
-
-
-
+                    if target_id != "53" and target_id != "43":
                         self.broadcast_code(target_id)  #Shutdown the player whos hit
+                        self.controller.frames['pregame_screen'].live_events.add_text(f'[-] {shooter.player_name} hit {target.player_name} | Shutting {target.player_name} down for 10 seconds')
+                        shooter.update_score(10)
+
                     else:
-                        if data == "53":
+                        if target_id == "53":
                             print("Red base has been scored")
                             print(" +100 points to the Green team")
-                            self.scrollframe_obj.add_text("[+] Red base has been scored +100 points to the Green team")
+                            self.controller.frames['pregame_screen'].live_events.add_text("[+] Red base has been scored +100 points to the Green team")
+                            shooter.update_score(100)
+                            self.broadcast_code(target_id)
 
-                        if data == "43":
+                        if target_id == "43":
                             print("Green base has been scored")
                             print(" +100 points to the Red team\n")
-                            self.scrollframe_obj.add_text("[+] Green base has been scored. +100 points to the Red team")
+                            self.controller.frames['pregame_screen'].live_events.add_text("[+] Green base has been scored. +100 points to the Red team")
+                            shooter.update_score(100)
+                            self.broadcast_code(target_id)
+
+                self.update_scores()
+
+
+
 
             except socket.error as e:
                 if self.running:
@@ -80,6 +108,7 @@ class Server:
     def broadcast_code(self, code):
         try:
             self.bcast_socket.sendto(code.encode("utf-8"), (self.bcast_addr, self.bcast_port))
+            print("Broadcasted Code: ",code)
         except socket.error as e:
             print(f"Broadcast error: {e}")
 
